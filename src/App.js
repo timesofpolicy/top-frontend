25b5c4077c8ea13252759a438ff6b3b29963eb9e
abhijeet-trojan <trojanabhijeet@gmail.com>
2023-02-26 19:27:08 +0530

    added client folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import PageLayout from './components/PageLayout';
+import { UserContextProvider } from './context/UserContext';
+import './index.css';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import LoginPage from './pages/LoginPage';
+import PostPage from './pages/PostPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<PageLayout />} />
+          {/* <Route index element={<Post />} /> */}
+          {/* <Route index element={<Index />}/> */}
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/register' element={<SignupPage />}/>
+          <Route path='/create' element={<CreatePost />}/>
+          <Route path='/post/:id' element={<PostPage />}/>
+          <Route path='/edit/:id' element={<EditPost />}/>
+        </Route>
+      </Routes> 
+    </UserContextProvider>
   );
 }
 
---
+++ src/pages/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from '../components/Post'
+
+const Index = () => {
+  return (
+    <>
+    <Post />
+    </>
+  )
+}
+
+export default Index
